95caeaee845606a1dcb193f26e7c8e7796580b2a
aarondill <aaronsacks2006@gmail.com>
2023-11-16 01:43:39 -0600

    hack: mark process.chdir as deprecated

--- src/run.ts
+++ src/run.ts
@@ -1,9 +1,22 @@
 #!/usr/bin/env zx
-import "zx/globals";
+declare global {
+	// eslint-disable-next-line @typescript-eslint/no-namespace
+	namespace NodeJS {
+		interface Process {
+			/**@deprecated Use zx.cd() instead */
+			// HACK: Mark chdir as deprecated, since it won't work with zx
+			chdir(directory: string): void;
+		}
+	}
+}
+
+// import "zx/globals";
+import { $ } from "zx";
 $.prefix = "set -euC -o pipefail;";
 
 async function main(args: string[]) {
 	void args;
+
 	return await Promise.resolve(0);
 }
 
