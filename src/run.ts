76b471536273aa897884c5accd232af43d6e167f
aarondill <aaronsacks2006@gmail.com>
2023-11-16 02:29:12 -0600

    allow main to return a promise or a value
    Just use Promise.resolved().

--- src/run.ts
+++ src/run.ts
@@ -1,23 +1,28 @@
 #!/usr/bin/env zx
+// {{{1
+// HACK: Mark chdir as deprecated, since it won't work with zx
 declare global {
 	// eslint-disable-next-line @typescript-eslint/no-namespace
 	namespace NodeJS {
 		interface Process {
 			/**@deprecated Use zx.cd() instead */
-			// HACK: Mark chdir as deprecated, since it won't work with zx
 			chdir(directory: string): void;
 		}
 	}
 }
-
-// import "zx/globals";
+// }}}
+import "zx/globals";
 import { $ } from "zx";
+
 $.prefix = "set -euC -o pipefail;";
 
 async function main(args: string[]) {
 	void args;
+	// Do some things!
 
 	return await Promise.resolve(0);
 }
 
-void main(process.argv.slice(2)).then(c => process.exit(c ?? 0));
+void Promise.resolve(main(process.argv.slice(2))).then(c =>
+	process.exit(c ?? 0)
+);
