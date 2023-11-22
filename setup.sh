7144570709d3225d55bfa6aa1ef22fa3188dcbb1
aarondill <aaronsacks2006@gmail.com>
2023-11-22 17:01:51 -0600

    add version detection to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euC -o pipefail
+MIN_NODE_MAJOR=18
 path_contains() {
   case ":$PATH:" in
   *":${1:-}:"*) return 0 ;;
@@ -12,6 +13,18 @@ log() { printf '%s\n' "$@" || true; }
 
 append_path "./node_modules/.bin"
 cd "${0%/*}" || true
+
+if has_cmd node; then
+  node_version="$(node --version)"
+  log "Node version $node_version is currently installed"
+  node_major="${node_version%%.*}"
+  node_major="${node_major#v}"
+  if [ "$node_major" -ge "$MIN_NODE_MAJOR" ]; then
+    log "Node v$node_major is >= $MIN_NODE_MAJOR! We have achieved setup."
+    exit 0
+  fi
+  log "Node version is not high enough."
+fi
 
 if ! has_cmd n; then
   log "Attempting to install n using a package manager!"
