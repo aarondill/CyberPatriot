8b2b70225c2c72ef794be5b4a4e43c1dd21df953
aarondill <aaronsacks2006@gmail.com>
2023-11-23 03:16:05 -0600

    add fix-apt action

---
+++ src/actions/fix-apt.ts
@@ -0,0 +1,56 @@
+import fs from "node:fs/promises";
+import { $, echo, which } from "zx";
+import type { Action } from "./index.js";
+import { error, isProcessOutput } from "../util/init.js";
+import { isWindows } from "../util/constants.js";
+import backup, { mapFile } from "../util/backup.js";
+import { useRoot } from "../util/root.js";
+
+// NOTE: because euid is changed, not uid, child processes are spawned as root!
+
+async function updateApt(apt: string) {
+	echo("Running apt update");
+	try {
+		await $`${apt} update`;
+		await $`${apt} upgrade`;
+	} catch (err) {
+		if (!isProcessOutput(err)) throw err;
+		echo("Command failed! " + err.message);
+		return false;
+	}
+}
+
+async function fixApt() {
+	if (isWindows) return true; // on windows this is a noop
+	const apt = await which("apt", { nothrow: true });
+	if (!apt) return error("Could not find apt!");
+
+	const sources = await fs
+		.readdir("/etc/apt/sources.list.d/")
+		.catch(() => null);
+	if (sources && sources.length > 0) {
+		echo(`Note: /etc/apt/sources.list.d/ contains ${sources.length} items:`);
+		for (const source of sources) {
+			echo(source);
+		}
+	}
+	await useRoot(backup, "/etc/apt/sources.list");
+	await useRoot(mapFile, "/etc/apt/sources.list", line => {
+		line = line.trim().replaceAll(/(\s){2,}/g, "$1"); // collapse whitespace
+		if (line.startsWith("deb cdrom:")) return `# ${line}`;
+		if (line.startsWith("#")) {
+			if (!line.includes("deb ")) return line; // keep comments
+			// Note the trailing space!
+			// Remove comment and whitespace
+			if (line.includes("ubuntu.com/ubuntu ")) return line.substring(2);
+
+			return line; // keep comments
+		}
+		return `# ${line}`; // Comment all other entries
+	});
+
+	return await updateApt(apt);
+}
+
+export const run = fixApt satisfies Action;
+export default fixApt satisfies Action;
