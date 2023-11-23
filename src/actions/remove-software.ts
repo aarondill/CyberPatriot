ccb4cdcbbc32d51c3f779ac249a9cebde25ac0a3
aarondill <aaronsacks2006@gmail.com>
2023-11-23 14:41:58 -0600

    add remove-software action
    only works for debian-based

---
+++ src/actions/remove-software.ts
@@ -0,0 +1,22 @@
+import { $ } from "zx";
+import type { Action } from "./index.js";
+import { isWindows } from "../util/constants.js";
+import { confirm } from "../util/flow.js";
+const commonProhibitedSoftware = ["ophcrack", "wireshark"];
+
+export async function run() {
+	if (!isWindows) return true;
+	for (const p of commonProhibitedSoftware) {
+		const { exitCode } = await $`dpkg-query -s -- ${p}`
+			.nothrow()
+			.quiet()
+			.stdio("ignore", "ignore", "ignore");
+		if (exitCode !== 0) continue; // This is not installed
+		if (!(await confirm(`Would you like to remove the ${p} package?`, true)))
+			continue;
+		await $`apt autopurge -- ${p}`;
+	}
+	return true;
+}
+
+export default run satisfies Action;
