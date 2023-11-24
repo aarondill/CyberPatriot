f9efa9a38918e24b38df12af97ec7710b619219d
aarondill <aaronsacks2006@gmail.com>
2023-11-24 03:40:43 -0600

    add tests to ensure that the action modules follow the right shapes

--- src/actions/windows/index.ts
+++ src/actions/windows/index.ts
@@ -1,3 +1,13 @@
+import type { Extends } from "tsafe";
+import { assert } from "tsafe";
 import { isWindows } from "../../util/index.js";
+import type { ActionModule } from "../index.js";
 // This module is a noop on non-Windows
 export const importChildren = isWindows;
+
+// If you are seeing an error on one of these lines, check the module referenced to make sure it matches the ActionModule interface!
+// NOTE: This list must be manually maintained!
+assert<Extends<typeof import("./index.js"), ActionModule>>();
+assert<Extends<typeof import("./automatic-updates.js"), ActionModule>>();
+assert<Extends<typeof import("./remove-software.js"), ActionModule>>();
+assert<Extends<typeof import("./remove-users.js"), ActionModule>>();
