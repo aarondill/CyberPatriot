7b29b4ef7e6244596aa19366c52ee9376a3e8021
aarondill <aaronsacks2006@gmail.com>
2023-11-24 03:24:45 -0600

    create util/types to seperate type utils

--- src/util/index.ts
+++ src/util/index.ts
@@ -5,27 +5,13 @@ export * from "./constants.js";
 export * from "./backup.js";
 export * from "./root.js";
 export * from "./generator.js";
+export * from "./types.js";
 
-import { ProcessOutput } from "zx";
 import { spawn, type SpawnOptions } from "node:child_process";
 import type { PathLike } from "node:fs";
 import type { FileHandle } from "node:fs/promises";
 import fs from "node:fs/promises";
-
-export function isProcessOutput(err: unknown): err is ProcessOutput {
-	return err instanceof ProcessOutput;
-}
-
-export function isNodeError(value: unknown): value is NodeJS.ErrnoException {
-	if (!(value instanceof Error)) return false;
-	if ("code" in value && typeof value.code !== "string") return false; // code?: string | undefined;
-	if ("path" in value && typeof value.path !== "string") return false; // path?: string | undefined;
-	if ("syscall" in value && typeof value.syscall !== "string") return false; // syscall?: string | undefined;
-	if ("errno" in value && typeof value.errno !== "number") return false; // errno?: number | undefined;
-	return true;
-}
-export const isPathLike = (x: unknown): x is PathLike =>
-	typeof x === "string" || x instanceof Buffer || x instanceof URL;
+import { isNodeError } from "./types.js";
 
 export const run = (args: readonly string[], options?: SpawnOptions) =>
 	spawn(args[0], args.slice(1), options ?? {});
---
+++ src/util/types.ts
@@ -0,0 +1,28 @@
+import type { PathLike } from "node:fs";
+import { isModuleNamespaceObject } from "node:util/types";
+import { assert } from "tsafe";
+import { ProcessOutput } from "zx";
+
+export function isProcessOutput(err: unknown): err is ProcessOutput {
+	return err instanceof ProcessOutput;
+}
+
+export function isNodeError(value: unknown): value is NodeJS.ErrnoException {
+	if (!(value instanceof Error)) return false;
+	if ("code" in value && typeof value.code !== "string") return false; // code?: string | undefined;
+	if ("path" in value && typeof value.path !== "string") return false; // path?: string | undefined;
+	if ("syscall" in value && typeof value.syscall !== "string") return false; // syscall?: string | undefined;
+	if ("errno" in value && typeof value.errno !== "number") return false; // errno?: number | undefined;
+	return true;
+}
+export const isPathLike = (x: unknown): x is PathLike =>
+	typeof x === "string" || x instanceof Buffer || x instanceof URL;
+
+export type ModuleNamespaceObject = Record<string, unknown>;
+export function assertDynamicImport(
+	module: unknown
+): asserts module is ModuleNamespaceObject {
+	const msg =
+		"import() returned non-object. This should be impossible. Is there a then() method?";
+	assert(isModuleNamespaceObject(module), msg);
+}
