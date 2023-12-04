c6ee6e3238ca4308500e71d4d78053e015804bf6
aarondill <aaronsacks2006@gmail.com>
2023-12-04 08:19:08 -0600

    remove references to __filename

--- src/actions/windows/remove-software.ts
+++ src/actions/windows/remove-software.ts
@@ -1,10 +1,11 @@
+import { fileURLToPath } from "url";
 import { warn } from "../../util/flow.js";
 import type { Action } from "../index.js";
-
+const filename = fileURLToPath(import.meta.url);
 export function run() {
-	warn(`"${__filename}" is not yet implemented`);
+	warn(`"${filename}" is not yet implemented`);
 	return true;
 }
 
-export const description = `An unimplemented action at ${__filename}.`;
+export const description = `An unimplemented action at ${filename}.`;
 export default run satisfies Action;
