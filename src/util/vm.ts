f756a0e84eed7d28daf8fa41553212104a74bd8c
aarondill <aaronsacks2006@gmail.com>
2023-12-05 13:15:33 -0600

    use openFile in vm.ts

--- src/util/vm.ts
+++ src/util/vm.ts
@@ -1,22 +1,20 @@
-import fs from "node:fs/promises";
 import { $ } from "zx";
 import { isWindows } from "./constants.js";
+import { openFile } from "./index.js";
 
 // TODO: Test this!
 async function isVMWindows() {
 	if (!isWindows)
 		throw new Error("isVMWindows called on a non-Windows platform");
 	// Do we need echo here?
-	const p = await $`(gwmi Win32_BaseBoard).Manufacturer`;
+	const p = await $`(gwmi Win32_BaseBoard).Manufacturer`.nothrow();
 	if (p.exitCode !== 0) return false;
 	return p.stdout.trim() === "Microsoft Corporation";
 }
 async function isVMUnix() {
 	if (isWindows) throw new Error("isVMUnix called on Windows");
 	// check for 'hypervisor'
-	let fd;
-	try {
-		fd = await fs.open("/proc/cpuinfo", "r");
+	return await openFile("/proc/cpuinfo", "r", async fd => {
 		for await (let line of fd.readLines()) {
 			if (!line.startsWith("flags")) continue;
 			line = line.substring(line.indexOf(":")); // remove `flags .*:`
@@ -24,9 +22,7 @@ async function isVMUnix() {
 			return line.includes(" hypervisor "); // check for hypervisor (not a substring, spaces!)
 		}
 		return false;
-	} finally {
-		await fd?.close();
-	}
+	});
 }
 
 // This function performs file operations. You should probably cache it!
