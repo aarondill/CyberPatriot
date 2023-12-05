e71bd3fa2a7380bfc0be5ac5f32e7f593626babb
aarondill <aaronsacks2006@gmail.com>
2023-12-05 12:25:14 -0600

    fix: make createGeneratorFromCallback work while using other async functions
    Wait until yeild returns (and _value) resolves before changing _value.

--- src/util/generator.ts
+++ src/util/generator.ts
@@ -19,15 +19,20 @@ export async function* createGeneratorFromCallback<
 	cb: (yeild: (v: Yield) => Promise<void>, ...args: Args) => PromiseLike<Ret>,
 	...args: Args
 ): AsyncGenerator<Yield, Ret> {
-	let _resolve: (value: Yield) => void,
-		_value = new Promise<Yield>(res => (_resolve = res));
+	let _resolve: (value: Yield) => void, // Return callback
+		_value = new Promise<Yield>(res => (_resolve = res)); // a promise that will resolve to the yeild value
+	let _resolveYeild: () => void = () => {},
+		_yeild = new Promise<void>(res => (_resolveYeild = res)); // a promise that will resolve when the value has been yeild
 	let done = false;
 
 	const yeildVal: YeildValueFunction<Yield> = async v => {
-		_resolve(v);
-		await _value;
-		_value = new Promise(res => (_resolve = res));
+		_resolve(v); // Resolve the value
+		await _yeild; // wait until the value has been used.
+		_yeild = new Promise<void>(res => (_resolveYeild = res)); // set up new yeild promise
+		_value = new Promise(res => (_resolve = res)); // set up new value promise
 	};
+
+	// Get a promise that resolves when the function is done.
 	const res = cb(yeildVal, ...args).then((v: Ret) => {
 		done = true;
 		return v;
@@ -36,7 +41,8 @@ export async function* createGeneratorFromCallback<
 	// This is fine. it will be changed in the above `.then()`
 	// eslint-disable-next-line no-unmodified-loop-condition
 	while (!done) {
-		yield _value;
+		yield _value; // yeild the value promise
+		_resolveYeild(); // mark the yeild promise as used -- allow _value to be changed in yeildVal
 	}
 	return await res;
 }
