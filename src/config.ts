fc1d6705e6bf77d638daa007e929485249139f42
aarondill <aaronsacks2006@gmail.com>
2023-12-07 12:47:10 -0600

    cleanup config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,47 +1,48 @@
 import fs from "node:fs/promises";
 import path from "node:path";
-import { isNodeError } from "./util/types.js";
-import yaml from "yaml";
-import { isNativeError } from "node:util/types";
+import type { DeepNullable } from "./util/types.js";
+import { NULLCB } from "./util/types.js";
+import yaml, { YAMLException } from "js-yaml";
 import { error, warn } from "./util/flow.js";
+export const CONFIG_FILE_NAME = "config.yaml";
 
 export type CloneOptions = {
+	/** URL to clone */
 	url: string;
-	// Options for the git clone command.
-	args?: null | string[];
+	/** Options for the git clone command. */
+	args?: string[];
 };
-export type YamlConfig = Partial<{
-	packages?: null | {
-		// Special case
-		"*"?: null | string[];
-		// id parsed from /etc/os-release
-		[id: string]: undefined | null | string[];
+export type YamlConfig = DeepNullable<{
+	packages: {
+		/** Special case. All platforms. */
+		"*": string[];
+		/** id parsed from /etc/os-release */
+		[id: string]: string[];
 	};
-	clone?: null | {
-		[path: string]: undefined | null | CloneOptions;
+	clone: {
+		[path: string]: CloneOptions;
 	};
 }>;
-export async function parseConfig(root: string): Promise<YamlConfig> {
-	const rc = path.join(root, "config.yaml");
-	const content = await fs.readFile(rc, { encoding: "utf8" }).catch(e => {
-		if (!isNodeError(e)) throw e;
-		return null;
-	});
-	if (!content) return {}; // file didn't exist (or not readable)
+
+async function parse(file: string): Promise<object> {
+	const content = await fs.readFile(file, { encoding: "utf8" }).catch(NULLCB);
+	if (content === null) return {}; // file didn't exist (or not readable)
 	let config;
 	try {
-		config = yaml.parse(content) as unknown;
+		config = yaml.load(content) ?? {};
 	} catch (e) {
-		const err = isNativeError(e) ? e.message : String(e);
-		error(`Could not parse yaml file '${rc}': ${err}`);
+		if (!(e instanceof YAMLException)) throw e;
+		error(`Could not parse yaml file '${file}': ${e.message}`);
 		return {};
 	}
-
-	if (!config) return {}; // empty file
 	if (typeof config !== "object") {
-		warn(`${rc} contains only a scalar. Ignoring...`);
+		warn(`${CONFIG_FILE_NAME} contains only a scalar. Ignoring...`);
 		return {};
 	}
-
+	return config;
+}
+export async function parseConfig(root: string): Promise<YamlConfig> {
+	const configPath = path.join(root, CONFIG_FILE_NAME);
+	const config = await parse(configPath);
 	return config;
 }
