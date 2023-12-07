9ef77e04a8434e53a29c9691277179d14a1d8627
aarondill <aaronsacks2006@gmail.com>
2023-12-07 10:25:45 -0600

    add commandStatus to output a warning if the command fails

--- src/util/index.ts
+++ src/util/index.ts
@@ -8,6 +8,18 @@ export * from "./root.js";
 export * from "./types.js";
 
 import { spawn, type SpawnOptions } from "node:child_process";
+import type { ProcessPromise } from "zx";
+import { ProcessOutput } from "zx";
+import { warn } from "./flow.js";
 
 export const run = (args: readonly string[], options?: SpawnOptions) =>
 	spawn(args[0], args.slice(1), options ?? {});
+
+export async function commandStatus(
+	command: ProcessPromise | ProcessOutput
+): Promise<ProcessOutput> {
+	const output =
+		command instanceof ProcessOutput ? command : await command.nothrow().run();
+	if (output.exitCode !== 0) warn("Command failed!");
+	return output;
+}
