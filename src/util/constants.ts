6a8496508d9a9e88621e103debd2a1b695cd128d
Aaron Dill <aaronsacks2006@gmail.com>
2024-10-24 23:33:25 -0500

    feat: add getApt function

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,1 +1,13 @@
+import { which } from "zx";
+
 export const isWindows = process.platform === "win32";
+
+// An array of commands that can be used the same as apt
+export const APT_CMDS = ["nala", "apt", "apt-get"];
+export async function getApt() {
+	// find a viable apt command
+	let apt = null;
+	for (const cmd of APT_CMDS)
+		if ((apt = await which(cmd, { nothrow: true }))) break;
+	return apt;
+}
