56b3d9b9534b75da0c7087cbb56036241c5cdee2
Aaron Dill <aaronsacks2006@gmail.com>
2024-10-24 22:12:02 -0500

    fix: actually set password policies

--- src/actions/linux/23-pass.ts
+++ src/actions/linux/23-pass.ts
@@ -1,14 +1,15 @@
 import fs from "fs/promises";
 import type { Action } from "../index.js";
 const findLine = (lines: string[], prop: string) =>
-	lines.findIndex(line => (line.match(/^\s*#?\s*(\S+)\s*/) ?? [])[1] === prop);
+	lines.findIndex(line => (line.match(/^\s*(\S+)\s*/) ?? [])[1] === prop);
 
 export async function run() {
 	const lines = (await fs.readFile("/etc/login.defs", "utf8")).split("\n");
 	const maxDaysLineI = findLine(lines, "PASS_MAX_DAYS");
-	if (maxDaysLineI !== -1) lines[maxDaysLineI] = `PASS_MAX_DAYS 5`;
+	lines[maxDaysLineI === -1 ? lines.length : maxDaysLineI] = `PASS_MAX_DAYS\t5`;
 	const minDaysLineI = findLine(lines, "PASS_MIN_DAYS");
-	if (minDaysLineI !== -1) lines[minDaysLineI] = `PASS_MIN_DAYS 1`;
+	lines[minDaysLineI === -1 ? lines.length : minDaysLineI] = `PASS_MIN_DAYS\t1`;
+	await fs.writeFile("/etc/login.defs", lines.join("\n"));
 }
 export const description = `Set password policies in /etc/login.defs`;
 export default run satisfies Action;
